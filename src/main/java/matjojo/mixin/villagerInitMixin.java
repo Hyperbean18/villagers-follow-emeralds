2e39f4b4758191cf5e8aab46f6c553fab48152f3
Hyperbean18 <79054980+Hyperbean18@users.noreply.github.com>
2021-06-21 15:43:51 +0530

    added deepslate emerald ore, switched to MerchantEntity

--- src/main/java/matjojo/mixin/villagerInitMixin.java
+++ src/main/java/matjojo/mixin/villagerInitMixin.java
@@ -2,7 +2,7 @@ package matjojo.mixin;
 
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.goal.TemptGoal;
-import net.minecraft.entity.passive.AbstractTraderEntity;
+import net.minecraft.entity.passive.MerchantEntity;
 import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.item.Items;
 import net.minecraft.recipe.Ingredient;
@@ -14,16 +14,16 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(VillagerEntity.class)
-public abstract class villagerInitMixin extends AbstractTraderEntity {
+public abstract class villagerInitMixin extends MerchantEntity {
 
-	public villagerInitMixin(EntityType<? extends AbstractTraderEntity> entityType, World world) {
+	public villagerInitMixin(EntityType<? extends MerchantEntity> entityType, World world) {
 		super(entityType, world);
 	}
 
 	@Inject(method = "<init>(Lnet/minecraft/entity/EntityType;Lnet/minecraft/world/World;Lnet/minecraft/village/VillagerType;)V",
-	at = @At(value = "TAIL"))
+			at = @At(value = "TAIL"))
 	private void inject(EntityType<? extends VillagerEntity> entityType, World world, VillagerType type, CallbackInfo ci) {
-		this.goalSelector.add(2, new TemptGoal(this, .4D, false, Ingredient.ofItems(Items.EMERALD_BLOCK, Items.EMERALD_ORE)));
+		this.goalSelector.add(2, new TemptGoal(this, .4D, Ingredient.ofItems(Items.EMERALD_BLOCK, Items.EMERALD_ORE,Items.DEEPSLATE_EMERALD_ORE), false));
 	}
 
 }
